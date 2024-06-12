7152960dba1154c721608a926dee603125a3708b
sellen <austin0801@naver.com>
2024-06-12 21:22:36 +0900

    install React JS & Tailwind CSS, Create The First Template

---
+++ client/src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import styled from "styled-components";
+
+const Div = styled.div`
+    font-size: 20px;
+`;
+
+const App = () => {
+    return <h1 className="text-3xl text-red-500">app</h1>;
+};
+
+export default App;
