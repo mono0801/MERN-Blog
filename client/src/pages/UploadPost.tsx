3c7a4e5728de2c0bb0dbdb804f920726dbd400de
sellen <austin0801@naver.com>
2024-06-27 17:42:41 +0900

    Implement Upload Post Img

--- client/src/pages/UploadPost.tsx
+++ client/src/pages/UploadPost.tsx
@@ -8,15 +8,42 @@ import { yupResolver } from "@hookform/resolvers/yup/src/yup.js";
 import { uploadPostSchema } from "./yup";
 import { useForm } from "react-hook-form";
 import { HiInformationCircle } from "react-icons/hi";
+import {
+    getDownloadURL,
+    getStorage,
+    ref,
+    uploadBytesResumable,
+} from "firebase/storage";
+import { app } from "../firebase";
+import { useSelector } from "react-redux";
+import { RootState } from "../redux/store";
+import { CircularProgressbar } from "react-circular-progressbar";
+import "react-circular-progressbar/dist/styles.css";
 
 interface IForm {
     title: string;
 }
 
+interface IUpload {
+    title: string;
+    category: string[];
+    imgUrl?: string;
+}
+
 const UploadPost = () => {
+    const { currentUser } = useSelector((state: RootState) => state.user);
+
     const [category, setCategory] = useState<string[] | null>(null);
     const [selectedCategory, setSelectedCategory] = useState<string[]>([]);
-    const [data, setData] = useState<IForm | null>(null);
+
+    const [file, setFile] = useState<File | null>(null);
+    const [imgUploadProgress, setImgUploadProgress] = useState<number | null>(
+        null
+    );
+    const [imgUrl, setImgUrl] = useState<string | undefined>(undefined);
+
+    const [data, setData] = useState<IUpload | null>(null);
+
     const [errMsg, setErrMsg] = useState<string | null>(null);
     const [loading, setLoading] = useState<boolean>(false);
     const {
@@ -27,7 +54,12 @@ const UploadPost = () => {
     } = useForm<IForm>({ resolver: yupResolver<IForm>(uploadPostSchema) });
 
     const handleValid = async (formData: IForm) => {
-        setData(formData);
+        const uploadDate: IUpload = {
+            title: formData.title,
+            category: selectedCategory,
+            imgUrl,
+        };
+        setData(uploadDate);
     };
 
     useEffect(() => {
@@ -54,6 +86,54 @@ const UploadPost = () => {
             selectedCategory.filter((selected) => selected !== option)
         );
     };
+
+    const handleUploadImg = async () => {
+        try {
+            if (!file) {
+                setErrMsg("Please Select an Image");
+                return;
+            }
+            setErrMsg(null);
+            const storage = getStorage(app);
+            const fileName = `post/${currentUser?.nickname}/${
+                file.name
+            }_${new Date().getTime()}`;
+            const storageRef = ref(storage, fileName);
+            const uploadTask = uploadBytesResumable(storageRef, file);
+            uploadTask.on(
+                "state_changed",
+                (snapshot) => {
+                    const progress =
+                        (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+                    setImgUploadProgress(Number(progress.toFixed(0)));
+                },
+                (_error) => {
+                    setErrMsg("Image Upload Failed");
+                    setImgUploadProgress(null);
+                },
+                () => {
+                    getDownloadURL(uploadTask.snapshot.ref).then(
+                        (downloadURL) => {
+                            setImgUploadProgress(null);
+                            setErrMsg(null);
+                            setImgUrl(downloadURL);
+                        }
+                    );
+                }
+            );
+        } catch (error) {
+            setErrMsg("Image Upload Failed");
+            setImgUploadProgress(null);
+            console.log(error);
+        }
+    };
+
+    useEffect(() => {
+        if (file) {
+            handleUploadImg();
+        }
+        return;
+    }, [file]);
 
     return (
         <div className="p-3 max-w-3xl mx-auto min-h-screen">
@@ -105,17 +185,43 @@ const UploadPost = () => {
                 </div>
 
                 <div className="flex gap-4 items-center justify-between border-4 border-teal-500 border-dotted p-3">
-                    <FileInput itemType="file" accept="image/*" />
+                    <FileInput
+                        itemType="file"
+                        accept="image/*"
+                        onChange={(e) =>
+                            setFile(e.target.files ? e.target.files[0] : null)
+                        }
+                    />
 
                     <Button
                         type="button"
                         gradientDuoTone={"purpleToBlue"}
                         size={"sm"}
                         outline
+                        onClick={handleUploadImg}
+                        disabled={imgUploadProgress ? true : false}
                     >
-                        Upload Image
+                        {imgUploadProgress !== null &&
+                        imgUploadProgress !== undefined ? (
+                            <div className="w-16 h-16">
+                                <CircularProgressbar
+                                    value={imgUploadProgress || 0}
+                                    text={`${imgUploadProgress}%`}
+                                />
+                            </div>
+                        ) : (
+                            "Upload Image"
+                        )}
                     </Button>
                 </div>
+
+                {imgUrl && (
+                    <img
+                        src={imgUrl}
+                        alt="upload"
+                        className="w-full h-72 object-cover"
+                    />
+                )}
 
                 <ReactQuill
                     theme="snow"
@@ -134,6 +240,15 @@ const UploadPost = () => {
                         icon={HiInformationCircle}
                     >
                         {errors.title.message}
+                    </Alert>
+                )}
+                {errMsg && (
+                    <Alert
+                        className="mt-3 font-semibold"
+                        color={"failure"}
+                        icon={HiInformationCircle}
+                    >
+                        {errMsg}
                     </Alert>
                 )}
             </form>
